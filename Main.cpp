4dc24c9991ffb237f332ba2c8f106f4d7ea699ba
Drew Chase <dcmanproductions@gmail.com>
2023-05-12 16:48:33 -0400

    added parse path and console input for path

--- Main.cpp
+++ Main.cpp
@@ -1,24 +1,57 @@
 // LFInteractive LLC. - All Rights Reserved
-
-/// Default
-#include <iostream>
 #include "Scanner.h"
+#include "Logger.h"
+#include <iostream>
+#include <chrono>
+
+void parse_path(string& path)
+{
+	if (path.front() == '"' && path.back() == '"')
+	{
+		path = path.substr(1, path.size() - 2);
+	}
+}
+void parse_arguments(int length, char* args[], vector<string>& arguments)
+{
+	for (int i = 0; i < length; i++)
+	{
+		string argument = args[i];
+		parse_path(argument);
+		arguments.push_back(argument);
+	}
+}
+
 int main(int length, char* args[])
 {
-	if (length > 0)
+	vector<string> arguments;
+	parse_arguments(length, args, arguments);
+
+	string path = "";
+	string filter = "";
+	if (length > 1)
 	{
-		vector<FileType*>* files;
-		if (length > 1)
+		path = string(args[1]);
+		if (length > 2)
 		{
-			files = Scanner::scan(string(args[0]), string(args[1]));
+			filter = string(args[2]);
 		}
-		else
-		{
-			files = Scanner::scan(string(args[0]));
-		}
-		Scanner::print(files);
-		return 0;
+	}
+	else
+	{
+		std::cout << "PATH: ";
+		std::getline(std::cin, path);
+		parse_path(path);
+		std::cout << "FILTER (LEAVE BLANK for NONE): ";
+		std::getline(std::cin, filter);
 	}
 
-	return 2;
+	auto start = std::chrono::high_resolution_clock::now();
+	Logger::log.info("Scanning '{}'", path);
+	Logger::log.warn("This might take a moment");
+	vector<FileType*>* files = Scanner::scan(path, filter);
+	Scanner::print(files);
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+	Logger::log.info("Process finished after {}ms", duration);
+	return 0;
 }
