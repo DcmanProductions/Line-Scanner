e2c7cb054094dc6a5a0f1d4a3eadaf0c5a0aaa3b
Drew Chase <dcmanproductions@gmail.com>
2023-05-12 02:38:15 -0400

    added entrypoint

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,20 @@
 // LFInteractive LLC. - All Rights Reserved
 
+/// Default
+#include <iostream>
+
+/// SPD Logger
+#include "Logger.h"
+
 int main(int length, char* args[])
 {
+	spdlog::logger log = Logger::log;
+
+	log.trace("Hello World");
+	log.debug("Hello World");
+	log.info("Hello World");
+	log.warn("Hello World");
+	log.error("Hello World");
+
 	return 0;
 }
