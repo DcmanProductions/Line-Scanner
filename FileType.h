11022deb84703e3338975825a997b7880c6bec89
Drew Chase <dcmanproductions@gmail.com>
2023-05-12 02:37:40 -0400

    added file types

---
+++ FileType.h
@@ -0,0 +1,13 @@
+// LFInteractive LLC. - All Rights Reserved
+#pragma once
+#include <string>
+using std::string;
+struct FileType
+{
+public:
+	string name;
+	string extension;
+	string path;
+	long lines;
+	long bytes;
+};
