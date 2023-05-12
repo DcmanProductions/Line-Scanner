4deea4f72ed4af4d9b042164318da14d6d690873
Drew Chase <dcmanproductions@gmail.com>
2023-05-12 16:46:35 -0400

    added print

--- Scanner.h
+++ Scanner.h
@@ -11,8 +11,24 @@ namespace fs = std::filesystem;
 class Scanner
 {
 public:
+	/**
+	 * This scans through all files and subdirectories in a path
+	 *
+	 * @param path This is the starting path of the scan
+	 */
 	static vector<FileType*>* scan(string path);
+	/**
+	 * This scans through all files and subdirectories in a path with a filter
+	 *
+	 * @param path This is the starting path of the scan
+	 * @param filter This will filter the results by the extension
+	 */
 	static vector<FileType*>* scan(string path, string filter);
+	/**
+	 * This will print the values of the FileTypes to the console using spdlog
+	 *
+	 * @param types This is the array of types
+	 */
 	static void print(vector<FileType*>* types);
 private:
 	static void get_files(string path, string filter);
